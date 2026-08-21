cae1053dd34393fd1006ba1fc98247053a376837
techbium <olatunjirauf091@gmail.com>
2026-08-21 07:24:50 +0100

    Fe2-HomePage...

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,3 +1,5 @@
+import './home.css';
+
 function Home(){
     return(
         <>
@@ -21,6 +23,7 @@ function Home(){
                 <h1>Whats is NDFF?</h1>
                 <article>
                     <p>The Nigeria Digital Future Forum brings together government, industry, academia, innovators, entrepreneurs, technology professionals and emerging talent to explore the technologies, partnerships and ideas shaping Nigeria's digital future.</p>
+                        {/* Make these six areas card/icons */}
                         <div>
                             <div>Artificial Intelligence</div>
                             <div>Cybersecurity</div>
@@ -33,10 +36,141 @@ function Home(){
             </section>
 
             <section>
-                    
+                <h1>Why attend?</h1>
+                <article>
+                     {/* I would make five cards: */}
+                    <div>
+                        <p>Government & Public Sector</p>
+                        <p>Engage industry leaders, technology providers and policymakers around digital transformation,e-Governance, cybersecurity and emerging technologies.</p>
+                    </div>
+
+                    <div>
+                        <p>Business & Industry Leaders</p>
+                        <p>Discover emerging solutions, meet decision-makers, build partnerships and explore opportunities created by Nigeria' digital transformation.</p>
+                    </div>
+
+                    <div>
+                        <p>Technology Professionals</p>
+                        <p>Learn from industry experts, discover emerging technologies and connect with organisations shaping the sector.</p>
+                    </div>
+
+                    <div>
+                        <p>Startups & Founders</p>
+                        <p>Showcase solutions, meet potential partners and customers, connect with investors and participate in the innovation ecosystem.</p>
+                    </div>
+
+                    <div>
+                        <p>Students & Emerging Talent</p>
+                        <p>Discover technology careers, participate in skills and innovation activities and connect with employers and industry professionals.</p>
+                    </div>
+                </article>    
             </section>
 
+            <section>
+                <h1>The Experience</h1>
+                <article>
+                    {/* I would make five cards: */}
+                    <div>
+                        <p>Government–Industry Dialogue</p>
+                        <p>Title</p>
+                        <p>Short description</p>
+                        <p>Format</p>
+                    </div>
 
+                    <div>
+                        <p>AI & Emerging Technology</p>
+                        <p>Title</p>
+                        <p>Short description</p>
+                        <p>Format</p>
+                    </div>
+                    
+                    <div>
+                        <p>Cybersecurity Forum</p>
+                        <p>Title</p>
+                        <p>Short description</p>
+                        <p>Format</p>
+                    </div>
+                    
+                    <div>
+                        <p>Innovation & Startup Exhibition</p>
+                        <p>Title</p>
+                        <p>Short description</p>
+                        <p>Format</p>
+                    </div>
+                    
+                    <div>
+                        <p>Student Innovation Challenge</p>
+                        <p>Title</p>
+                        <p>Short description</p>
+                        <p>Format</p>
+                    </div>
+                    
+                    <div>
+                        <p>Career & Skills Hub</p>
+                        <p>Title</p>
+                        <p>Short description</p>
+                        <p>Format</p>
+                    </div>
+                    
+                    <div>
+                        <p>Networking</p>
+                        <p>Title</p>
+                        <p>Short description</p>
+                        <p>Format</p>
+                    </div>
+
+                    {/* 
+                        ---Each card can have:---
+                            Title
+                            Short description
+                            Format
+                    */}
+
+                    {/* 
+                        AI & Emerging Technology
+
+                        Practical conversations and demonstrations around
+                        Artificial Intelligence and emerging technologies.
+
+                        Keynotes • Panels • Demonstrations • Showcases 
+                    */}
+
+                </article>
+            </section>
+
+            <section>
+                <h1>Three-Day Programme</h1>
+                <article>
+                    <div>
+                        <h3>Day1</h3>
+                        <p>Arrival, Accreditation & Official Opening</p>
+                        <ul>
+                            <li></li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h3>Day2</h3>
+                        <p>Government, Industry & Technology</p>
+                        <ul>
+                            <li></li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h3>Day3</h3>
+                        <p>Innovation, Digital Talent & Closing</p>
+                        <ul>
+                            <li></li>
+                        </ul>
+                    </div>
+                    
+                </article>
+                    <div>
+                        <button> EXPLORE FULL PROGRAMME </button>
+                    </div>
+                {/* Then list the activities beneath each. */}
+            </section>
         </>
     )
 }
