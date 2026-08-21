85bf0b0c397b8dfaf7ff1e71415eeae22f90c05a
techbium <olatunjirauf091@gmail.com>
2026-08-21 18:42:43 +0100

    Merge-registration and nav wwith home...

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import {ROUTES} from './route.js';
 // import './index.css';
 import Home from './pages/home.jsx';
+import Registration from './pages/Registration.jsx';
 
 function AppRoute(){
     return (
         <Routes>
             <Route path={ROUTES.HOME} element={<Home/>}/>
             <Route path='/' element={<Home/>}/>
+            <Route path={ROUTES.REGISTRATION} element={<Registration/>}/>
         </Routes>
     )
 }
